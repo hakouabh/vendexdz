5baf3ea082b9edc50d7fd6922bca4e94e2ea0d4f
abdelhak megherbi <megherbi.abdelhak1@gmail.com>
2026-02-15 11:12:34 +0100

    handleLightFunnelsWebhook

--- app/Http/Controllers/Webhook/OrderWebhookController.php
+++ app/Http/Controllers/Webhook/OrderWebhookController.php
@@ -119,18 +119,7 @@ protected function handleFoorwebWebhook(Request $request, $platform)
      */
     protected function handleLightFunnelsWebhook(Request $request, $platform)
     {
-        try {
-            $logFile = storage_path('logs/raw_http_requests.txt');
-            $rawHttpContent = $request;
-
-            file_put_contents($logFile, $rawHttpContent, FILE_APPEND);
-        } catch (\Exception $e) {
-            Log::error("Failed to write raw request to file: " . $e->getMessage());
-        }
         $payload = $request->all();
-
-        Log::info("LightFunnels Webhook Processing", ['order_name' => $payload['node']['name'] ?? 'unknown']);
-
 
         $orderData = $payload['node'] ?? [];
 
@@ -335,16 +324,66 @@ protected function extractWilayaFromLightFunnels($address)
     if (is_numeric($state)) {
         return (int) $state;
     }
-
    
     $map = [
-        'alger' => 16, 'algiers' => 16, 'الجزائر' => 16,
-        'oran' => 31, 'وهران' => 31,
-        'blida' => 9, 'البليدة' => 9,
-        'khemis miliana' => 44, 'ain defla' => 44, 'عين الدفلى' => 44,
-        'chlef' => 02, 'الشلف' => 02,
-        'medea' => 26, 'المدية' => 26,
-        // أضف المزيد هنا...
+        'Adrar' => '01' ,
+        'Chlef' => '02' ,
+        'Laghouat' => '03' ,
+        'Oum El Bouaghi' => '04' ,
+        'Batna' => '05' ,
+        'Béjaïa' => '06' ,
+        'Biskra' => '07' ,
+        'Béchar' => '08' ,
+        'Blida' => '09' ,
+        'Bouira' => '10' ,
+        'Tamanrasset' => '11' ,
+        'Tébessa' => '12' ,
+        'Tlemcen' => '13' ,
+        'Tiaret' => '14' ,
+        'Tizi Ouzou' => '15' ,
+        'Alger' => '16' ,
+        'Djelfa' => '17' ,
+        'Jijel' => '18' ,
+        'Sétif' => '19' ,
+        'Saïda' => '20' ,
+        'Skikda' => '21' ,
+        'Sidi Bel Abbès' => '22' ,
+        'Annaba' => '23' ,
+        'Guelma' => '24' ,
+        'Constantine' => '25' ,
+        'Médéa' => '26' ,
+        'Mostaganem' => '27' ,
+        'M’Sila' => '28' ,
+        'Mascara' => '29' ,
+        'Ouargla' => '30' ,
+        'Oran' => '31' ,
+        'El Bayadh' => '32' ,
+        'Illizi' => '33' ,
+        'Bordj Bou Arreridj' => '34' ,
+        'Boumerdès' => '35' ,
+        'El Tarf' => '36' ,
+        'Tindouf' => '37' ,
+        'Tissemsilt' => '38' ,
+        'El Oued' => '39' ,
+        'Khenchela' => '40' ,
+        'Souk Ahras' => '41' ,
+        'Tipaza' => '42' ,
+        'Mila' => '43' ,
+        'Aïn Defla' => '44' ,
+        'Naâma' => '45' ,
+        'Aïn Témouchent' => '46' ,
+        'Ghardaïa' => '47' ,
+        'Relizane' => '48' ,
+        'Timimoun' => '49' ,
+        'Bordj Badji Mokhtar' => '50' ,
+        'Ouled Djellal' => '51' ,
+        'Béni Abbès' => '52' ,
+        'In Salah' => '53' ,
+        'In Guezzam' => '54' ,
+        'Touggourt' => '55' ,
+        'Djanet' => '56' ,
+        'El M’Ghair' => '57' ,
+        'El Meniaa' => '58' ,
     ];
 
     $search = mb_strtolower(trim($state . ' ' . $city));
