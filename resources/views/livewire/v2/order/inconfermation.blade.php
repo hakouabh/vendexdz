9fa20847004ca69c88ac588bb7032274ba8c99f4
abdelhak megherbi <megherbi.abdelhak1@gmail.com>
2026-03-29 10:45:44 +0100

    Filter Product align left

--- resources/views/livewire/v2/order/inconfermation.blade.php
+++ resources/views/livewire/v2/order/inconfermation.blade.php
@@ -115,7 +115,7 @@
 
 
             <!-- PRODUCT FILTER -->
-            <x-dropdown align="center" width="48">
+            <x-dropdown align="left" width="48">
                 <x-slot name="trigger">
                     <button type="button"
                         class="inline-flex items-center px-3 py-2 border border-gray-300 text-sm rounded-md text-gray-500 bg-white hover:text-gray-700 transition">
@@ -174,9 +174,6 @@
             </x-dropdown>
 
         </div>
-
-
-
         <!-- RIGHT ACTIONS -->
         <div class="flex flex-wrap gap-2 justify-start lg:justify-end w-full lg:w-2/5">
 
--- resources/views/livewire/v2/order/indelivery.blade.php
+++ resources/views/livewire/v2/order/indelivery.blade.php
@@ -117,7 +117,7 @@
 
 
             <!-- PRODUCT FILTER -->
-            <x-dropdown align="center" width="48">
+            <x-dropdown align="left" width="48">
                 <x-slot name="trigger">
                     <button type="button"
                         class="inline-flex items-center px-3 py-2 border border-gray-300 text-sm rounded-md text-gray-500 bg-white hover:text-gray-700 transition">
