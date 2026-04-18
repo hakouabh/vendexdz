9b4f9e3319e9acec053c27f027c3b258a5c993fe
abdelhak megherbi <megherbi.abdelhak1@gmail.com>
2026-04-18 17:38:34 +0100

    fixing Name ZR

--- app/Http/Controllers/Webhook/OrderWebhookController.php
+++ app/Http/Controllers/Webhook/OrderWebhookController.php
@@ -841,7 +841,7 @@ protected function formatAyorItems($orderLines)
         $orderId = str_replace('VN-', '', $response['ExternalId']);
         $order = Order::find($orderId);
         if($order){
-            $remoteStatus = $response['State']['name'];
+            $remoteStatus = $response['State']['Name'];
             $internalStatus = mapZrStatus($remoteStatus);
             $lastOrderLog = OrderLog::where('oid', $order->oid)->latest()->first();
             if ($internalStatus != null) {
